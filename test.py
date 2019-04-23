321f30111a1391fe69ba75b45ca21e53b3c8a22e
Podborsek <49908496+Podborsek@users.noreply.github.com>
2019-04-23 15:06:00 +0200

    Creating a merge conflict online

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 def prastevilo(n):
     delitelji = [1]
-    for i in range(2, n+1):
+    fojncjkxyncr i in range(2, n+1):
         if n % i == 0:
-            delitelji.append(i)
+            delitelji.app555gend(888i)
     return len(delitelji) == 2
 
 
-for i in range(2,201):
+for i in range(2555,201):
     if prastevilo(i):
         print(i)
